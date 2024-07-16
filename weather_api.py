56500a03362ce727dac63a2dc190c08d5f879760
duongve13112002 <71595470+duongve13112002@users.noreply.github.com>
2024-07-16 21:24:20 +0700

    Update weather_api.py

--- weather_api.py
+++ weather_api.py
@@ -14,16 +14,16 @@ API_KEY = 'your_api'
 weather_cache = {}
 date_check = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-def get_weather_data(city = "",lat = "",lon = ""):
+def get_weather_data(city = "",lat = "",lon = "",day = 5):
     if city !="":
-        url = f'http://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={city}&days=5'
+        url = f'http://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={city}&days={day}'
         response = requests.get(url)
         if response.status_code == 200:
             return response.json()
         else:
             return None
     elif lat != "" and lon != "":
-        url = f'http://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={lat},{lon}&days=5'
+        url = f'http://api.weatherapi.com/v1/forecast.json?key={API_KEY}&q={lat},{lon}&days={day}'
         response = requests.get(url)
         if response.status_code == 200:
             return response.json()
@@ -31,12 +31,38 @@ def get_weather_data(city = "",lat = "",lon = ""):
             return None
     return None
 
+def more_weather_data(city = '',lat = '',lon = ''):
+    global weather_cache
+    #print(weather_cache[lat+lon])
+    if city != '':
+        wanted_day = weather_cache[city]['days'] + 4
+        weather_cache.pop(city,None)
+        data = get_weather_data(city = city, day = wanted_day)
+        weather_cache[city] ={
+            'data': data,
+            'timestamp': datetime.now(),
+            'days' : wanted_day
+        }
+        return data 
+    elif lat != '' and lon != '':
+        wanted_day = weather_cache[lat+lon]['days'] + 4
+        weather_cache.pop(lat+lon,None)
+        data = get_weather_data(lat = lat, lon = lon, day = wanted_day)
+        weather_cache[lat+lon] ={
+            'data': data,
+            'timestamp': datetime.now(),
+            'days' : wanted_day
+        }
+        return data 
+    return None
+
 def save_weather_data(city, data):
     global weather_cache
     timestamp = datetime.now()
     weather_cache[city] = {
         'data': data,
-        'timestamp': timestamp
+        'timestamp': timestamp,
+        'days' : 5
     }
 
 def get_cached_weather_data(city):
